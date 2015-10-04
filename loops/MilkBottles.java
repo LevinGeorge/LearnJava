fba69eb4c01749d155305d19eb0f539ad5f95772
Levin <levingeorge41@gmail.com>
2015-10-04 01:35:31 +0100

    Added another extra exercise

---
+++ loops/MilkBottles.java
@@ -0,0 +1,22 @@
+/**
+ * @author : Levin George
+ * Here, we learn about for-loops.
+ * for(initialisation, termination,increment) {}
+ **/
+
+import java.util.Scanner;
+
+ public class MilkBottles {
+ 	public static void main(String[] args) {
+ 		
+ 		Scanner scan = new Scanner(System.in);
+ 		System.out.println("How many bottles do you have?");
+ 		int bottles = scan.nextInt();
+
+ 		for(int i =bottles;i>0;i--)
+ 		{
+ 			System.out.println(i+" bottles of milk on the wall...");  //The %d is replaced by the variable 'i'
+ 		}
+ 	}
+	
+}
